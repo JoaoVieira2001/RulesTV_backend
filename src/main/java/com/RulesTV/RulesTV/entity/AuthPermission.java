ec74dcc8c22819bde0da121e1f7f37f5368a7893
JoaoVieira2001 <joaoedrvieira01@gmail.com>
2025-04-11 10:36:04 +0100

    Add of entity AuthPermission in backend

---
+++ src/main/java/com/RulesTV/RulesTV/entity/AuthPermission.java
@@ -0,0 +1,62 @@
+package com.RulesTV.RulesTV.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class AuthPermission {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(name= "codename",nullable = false)
+    private String codename;
+
+    @Column(name= "content_type_id",nullable = false)
+    private Integer contentTypeId;
+
+    public AuthPermission(Integer id, String name, String codename, Integer contentTypeId) {
+        this.id = id;
+        this.name = name;
+        this.codename = codename;
+        this.contentTypeId = contentTypeId;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCodename() {
+        return codename;
+    }
+
+    public void setCodename(String codename) {
+        this.codename = codename;
+    }
+
+    public Integer getContentTypeId() {
+        return contentTypeId;
+    }
+
+    public void setContentTypeId(Integer contentTypeId) {
+        this.contentTypeId = contentTypeId;
+    }
+}
---
+++ src/main/java/com/RulesTV/RulesTV/repositories/AuthPermissionRepository.java
@@ -0,0 +1,7 @@
+package com.RulesTV.RulesTV.repositories;
+
+import com.RulesTV.RulesTV.entity.AuthPermission;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthPermissionRepository extends JpaRepository<AuthPermission,Integer> {
+}
